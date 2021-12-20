0a35a915e34c0fa5c5ca1cd9e9422bfe3e72cd71
hung2504 <vanhung25042000@gmail.com>
2021-12-20 18:41:49 +0700

    Finish Câu 1-a

---
+++ app/src/main/java/com/example/a09_18079021_nguyenvanhung_422000419502_nhom01/RegisterScreem.java
@@ -0,0 +1,14 @@
+package com.example.a09_18079021_nguyenvanhung_422000419502_nhom01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterScreem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_screem);
+    }
+}
